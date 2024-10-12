3441de766b35ee02f0ae65dbb88bd3293b943c51
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-12 23:50:03 +0500

    Add files via upload

--- arr2.cpp
+++ arr2.cpp
@@ -52,17 +52,32 @@ int main(){
 // }
 // cout<<"The minimum element is : "<<largest;
 
-int age[]={23,21,-4,9,-23,12,-34};
-int size =7;
-int smallest=INT_MAX;
-int largest=INT_MIN;
-for(int i=0;i<size;i++)
-{
-  smallest=min(age[i],smallest);
-  largest=max(age[i],largest);
-}
-cout<<"Size of an array is : "<<size<<endl;
-cout<<"Smallest element is : "<<smallest<<endl;
-cout<<"Largest element is : "<<largest;
+// int age[]={23,21,-4,9,-23,12,-34};
+// int size =7;
+// int smallest=INT_MAX;
+// int largest=INT_MIN;
+// for(int i=0;i<size;i++)
+// {
+//   smallest=min(age[i],smallest);
+//   largest=max(age[i],largest);
+// }
+// cout<<"Size of an array is : "<<size<<endl;
+// cout<<"Smallest element is : "<<smallest<<endl;
+// cout<<"Largest element is : "<<largest;
+
+// int age[]={23,34,5,6,7,8};
+// int size=6;
+// age[size];
+// int sum = 0; 
+// int pro=1;
+// for(int i=0;i<size;i++)
+// {
+//   sum +=age[i];
+//   pro *=age[i];
+// }
+// cout<<"The sum  of an array are : "<<sum<<endl;
+// cout<<"The product  an array are : "<<pro;
+
+
 
 }
