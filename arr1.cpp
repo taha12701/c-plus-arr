1e4df75cae74e347bbf790924774a6ecd304d54a
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-12 14:00:25 +0500

    Add files via upload

---
+++ arr1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+  // int marks[5]={46,67,54,12,19};
+  // marks[3]=67;
+  // cout<<marks[3];
+
+  //loops on an Array
+
+  // int marks[5]={46,67,54,12,19};
+  // int sz=sizeof(marks)/ sizeof(int);
+  // cout<<sz;
+
+  // int size=5;
+  // int marks[size];
+  // cout<<"Enter the elements in an array : "<<endl;
+  // for(int i=0;i<size;i++)
+  // {
+  //   cin>>marks[i];
+  // }
+
+
+  // cout<<"The elements of an array are : "<<endl;
+  // for(int i=0;i<size;i++)
+  // {
+  //   cout<<marks[i]<<endl;
+  // }
+
+
+  // int marks[]={23,43,34,56};
+
+  // cout<<sizeof(marks)<<endl;
+  // cout<<sizeof(int);
+
+
+  // 
+
+}
